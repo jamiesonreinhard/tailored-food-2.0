16d1f810335588b444c721e1005da8d828fbce42
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2023-11-15 14:22:17 -0600

    add contentful library

--- src/pages/api/contentful.js
+++ src/pages/api/contentful.js
@@ -1,19 +1,14 @@
-import axios from 'axios';
-
-const contentfulAPI = axios.create({
-  baseURL: 'https://cdn.contentful.com',
+const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  environment: "master",
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
 
-export const fetchBlogPosts = async () => {
-  try {
-    const response = await contentfulAPI.get(
-      `/spaces/${process.env.CONTENTFUL_SPACE_ID}/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=blogPost`
-    );
-    return response.data.items;
-  } catch (error) {
-    console.error('Error fetching blog posts from Contentful:', error);
-    throw error;
-  }
+export const fetchBlogPosts = () => {
+  client
+    .getContentType("<content_type_id>")
+    .then((contentType) => console.log(contentType))
+    .catch(console.error);
 };
 
 // Create similar functions for other content types (media posts, galleries, video galleries)
