3d55a92b63cced7beec1826bd30b3b825619a1d1
Jamieson Reinhard <jamieson@cpgexperience.com>
2024-01-12 06:48:50 -0600

    fix GTM

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,27 @@
 import '@/styles/globals.css'
 import Head from 'next/head'
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Function to initialize GTM
+    const initializeGTM = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', 'G-MCPQ21FQH2', {
+        page_path: window.location.pathname,
+      });
+    }
+
+    // Load GTM script
+    const script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-MCPQ21FQH2';
+    script.async = true;
+    script.onload = initializeGTM;
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <div>
       <Head>
